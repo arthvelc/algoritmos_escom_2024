3b0b875ab7ce9a3a30a9b2436d2858b5f792759e
arthvelc <asolares.devml@gmail.com>
2024-05-08 11:56:21 -0600

    ya quedó el algoritmo de huffman implementando programación orientada a objetos

---
+++ practica_lab_7/README.md
@@ -0,0 +1,38 @@
+Aquí tienes el README actualizado para tu práctica 7:
+
+# Práctica 7
+
+## Equipo
+
+- Solis Lugo Mayra
+- Solares Velasco Arturo Misael
+
+Este repositorio contiene la implementación de un programa en Python que realiza la codificación y decodificación de Huffman para un texto dado.
+
+## Codificación de Huffman
+
+Este programa permite al usuario ingresar un texto y luego realiza los siguientes pasos:
+
+1. Genera las frecuencias de ocurrencia de cada letra en el texto, incluyendo los espacios.
+2. Utiliza las frecuencias para construir un árbol de Huffman.
+3. Genera los códigos de Huffman para cada letra.
+4. Codifica el texto original utilizando los códigos de Huffman y lo guarda en un archivo llamado "texto_codificado.txt".
+5. Realiza la decodificación del texto codificado utilizando el árbol de Huffman y muestra el texto decodificado en la consola.
+
+## Uso del Programa
+
+Para utilizar el programa, sigue estos pasos:
+
+1. Ejecuta el programa en un entorno Python.
+2. Ingresa el texto que deseas codificar cuando se solicite.
+3. El programa generará dos archivos: "texto_original.txt", que contiene el texto original ingresado, y "texto_codificado.txt", que contiene el texto codificado utilizando el algoritmo de Huffman.
+4. El programa también imprimirá en la consola el texto decodificado, para verificar que el proceso de codificación y decodificación se haya realizado correctamente.
+
+## Referencias
+
+El programa utiliza el algoritmo de codificación y decodificación de Huffman, que es una técnica de compresión de datos ampliamente utilizada. Se implementó siguiendo los principios y algoritmos descritos en la literatura estándar sobre codificación de Huffman.
+
+## Notas Adicionales
+
+Este programa no requiere la instalación de dependencias externas y utiliza solo las bibliotecas estándar de Python, por lo que se puede ejecutar sin problemas en cualquier entorno de Python estándar.
+
--- practica_lab_7/huffman.py
+++ practica_lab_7/huffman.py
@@ -1,76 +1,107 @@
-'''Parte 1. Codificación de Huffman.
-Genera un programa que pida al usuario capturar un texto. Dicho texto tendrá que ser procesado de tal manera que se generen las frecuencias de ocurrencia de cada letra (incluidos los espacios), posteriormente genera la codificación de Huffman correspondiente y codifica el texto capturado. 
+import heapq
+import os
 
-Almacena el texto original y codificado en archivos separados.
+'''Utilizaré la programación orientada a objetos para implementar el algoritmo de Huffman.
 
-Parte 2: Decodificación Huffman.
+También utilizaré la librería heapq para manejar la cola de prioridad necesaria para el algoritmo.
 
-Averigua en qué consiste el algoritmo de decodificación del código Huffman, e incorpóralo en el programa de la primera parte. Prueba con el mismo texto de entrada.'''
+También la librería os para manejar la lectura y escritura de archivos.'''
 
-def huffman(texto):
-    # Genera la tabla de frecuencias
-    tabla = {}
-    for letra in texto:
-        if letra in tabla:
-            tabla[letra] += 1
-        else:
-            tabla[letra] = 1
 
-    print(f'Tabla de frecuencias: {tabla}')
-    # Genera el árbol de Huffman
-    arbol = []
+# Clase NodoHuffman: Representa un nodo en el árbol de Huffman.
+class NodoHuffman:
+    # Atributos del nodo Huffman
+    def __init__(self, caracter, frecuencia):
+        self.caracter = caracter
+        self.frecuencia = frecuencia
+        self.izquierda = None
+        self.derecha = None
 
-    for letra in tabla:
-        arbol.append((tabla[letra], letra))
-    arbol.sort()
+    # Método __lt__: Compara dos nodos Huffman por frecuencia.
+    def __lt__(self, otro):
+        return self.frecuencia < otro.frecuencia
 
-    print(f'Árbol de Huffman: {arbol}')
-    while len(arbol) > 1:
-        peso1, letra1 = arbol.pop(0)
-        peso2, letra2 = arbol.pop(0)
-        arbol.append((peso1 + peso2, letra1 + letra2))
-        arbol.sort()
 
-    print(f'Árbol de Huffman: {arbol}')
-    arbol = arbol[0][1]
-    print(f'Árbol de Huffman: {arbol}')
+# Clase CodificadorHuffman: Codifica y decodifica texto utilizando el algoritmo de Huffman.
+class CodificadorHuffman:
+    # Genera un diccionario de frecuencias de caracteres en un texto.
+    def generar_frecuencias(self, texto):
+        frecuencias = {}
+        for caracter in texto:
+            if caracter in frecuencias:
+                frecuencias[caracter] += 1
+            else:
+                frecuencias[caracter] = 1
+        return frecuencias
 
-    # Genera la tabla de códigos
-    tabla = {}
-    for letra in arbol:
-        
-        tabla[letra] = ''
-    print(f'Tabla de códigos: {tabla}')
-# Tengo que ver que pedo con la tabla de códigos siempre se va a == 1
-    for letra in arbol:
-        if len(letra) == 1:
-            tabla[letra] = '0'
-        else:
-            for i in letra:
-                tabla[i] = '1' + tabla[i]
+    # Genera un árbol de Huffman a partir del diccionario de frecuencias.
+    def generar_arbol_huffman(self, frecuencias):
+        cola_prioridad = [NodoHuffman(caracter, frecuencia) for caracter, frecuencia in frecuencias.items()]
+        heapq.heapify(cola_prioridad)
+        while len(cola_prioridad) > 1:
+            nodo_izq = heapq.heappop(cola_prioridad)
+            nodo_der = heapq.heappop(cola_prioridad)
+            nodo_combinado = NodoHuffman(None, nodo_izq.frecuencia + nodo_der.frecuencia)
+            nodo_combinado.izquierda = nodo_izq
+            nodo_combinado.derecha = nodo_der
+            heapq.heappush(cola_prioridad, nodo_combinado)
+        return cola_prioridad[0]
     
-    print(f'Tabla de códigos: {tabla}')
+    # Genera un diccionario de códigos de Huffman a partir del árbol de Huffman.
+    def generar_codigos_huffman(self, nodo, prefijo="", codigos={}):
+        if nodo is not None:
+            if nodo.caracter is not None:
+                codigos[nodo.caracter] = prefijo
+            self.generar_codigos_huffman(nodo.izquierda, prefijo + "0", codigos)
+            self.generar_codigos_huffman(nodo.derecha, prefijo + "1", codigos)
+        return codigos
 
-    # Codifica el texto
-    texto_codificado = ''
-    for letra in texto:
-        texto_codificado += tabla[letra]
-    
-    #return texto_codificado
+    # Codifica un texto utilizando un diccionario de códigos de Huffman.
+    def codificar_texto(self, texto, codigos):
+        texto_codificado = ""
+        for caracter in texto:
+            texto_codificado += codigos[caracter]
+        return texto_codificado
 
-def decodifica_huffman(texto_codificado, arbol):
-    texto = ''
-    nodo = arbol
-    for bit in texto_codificado:
-        if bit == '0':
-            nodo = nodo[0]
-        else:
-            nodo = nodo[1]
-        if len(nodo) == 1:
-            texto += nodo
-            nodo = arbol
-    return texto
+    # Decodifica un texto codificado utilizando un árbol de Huffman.
+    def decodificar_texto(self, texto_codificado, arbol_huffman):
+        texto_decodificado = ""
+        nodo_actual = arbol_huffman
+        for bit in texto_codificado:
+            if bit == '0':
+                nodo_actual = nodo_actual.izquierda
+            else:
+                nodo_actual = nodo_actual.derecha
+            if nodo_actual.caracter is not None:
+                texto_decodificado += nodo_actual.caracter
+                nodo_actual = arbol_huffman
+        return texto_decodificado
 
-if __name__ == '__main__':
-    texto = input('Introduce un texto: ')
-    huffman(texto)
+# Función main: Pide al usuario un texto, lo codifica y lo decodifica.
+def main():
+    codificador = CodificadorHuffman()
+
+    # Parte 1: Codificación de Huffman
+    texto = input("Introduce el texto a codificar: ")
+    frecuencias = codificador.generar_frecuencias(texto)
+    arbol_huffman = codificador.generar_arbol_huffman(frecuencias)
+    codigos = codificador.generar_codigos_huffman(arbol_huffman)
+    texto_codificado = codificador.codificar_texto(texto, codigos)
+
+    with open("./practica_lab_7/texto_original.txt", "w") as archivo_original:
+        archivo_original.write(texto)
+
+    with open("./practica_lab_7/texto_codificado.txt", "w") as archivo_codificado:
+        archivo_codificado.write(texto_codificado)
+
+    # Parte 2: Decodificación de Huffman
+    texto_codificado_leido = ""
+    with open("./practica_lab_7/texto_codificado.txt", "r") as archivo_codificado:
+        texto_codificado_leido = archivo_codificado.read()
+
+    texto_decodificado = codificador.decodificar_texto(texto_codificado_leido, arbol_huffman)
+    print("Texto decodificado:", texto_decodificado)
+
+if __name__ == "__main__":
+    main()
+
